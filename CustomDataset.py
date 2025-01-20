c171225d601d82c77fba591215e3ca7bbeb5b208
Brendan Raines <brendan.raines@gmail.com>
2025-01-20 20:26:00 +1100

    Adding files via upload

---
+++ CustomDataset.py
@@ -0,0 +1,76 @@
+import zarr
+import numpy as np
+import torch
+from torch.nn.functional import pad
+from glob import glob
+from torch.utils.data import Dataset
+from random import randint
+
+class CustomTomogramDataset(Dataset):
+    def __init__(
+        self,
+        images_path: str,
+        source_tomograms: int = 2,
+        target_tomograms: int = 1,
+        transform:object= None,
+        target_transform:object= None,
+        random_get:bool = False
+    ):
+        self.tomograms = sorted(glob(images_path))
+        self.target_tomograms = target_tomograms
+        self.source_tomograms = source_tomograms
+        self.transform = transform
+        self.target_transform = target_transform
+        self.random_get = random_get
+
+    def __len__(self) -> int:
+        return len(self.tomograms)
+
+    def __getitem__(self, idx: int) -> tuple[np.array, np.array]:
+        self.f = zarr.open(self.tomograms[idx])
+        #print(type(self.f))
+
+        if self.random_get:
+            self.source_tomograms = randint(1,2)
+            self.target_tomograms = self.source_tomograms - 1
+
+        source = np.array(self.f.get(self.source_tomograms))
+        target = np.array(self.f.get(self.target_tomograms))
+
+        if self.transform:
+            source, target = self.transform(source, target)
+
+        target = torch.from_numpy(target[np.newaxis,...].copy())
+        source = torch.from_numpy(source[np.newaxis,...].copy())
+        
+        
+        source_shape, target_shape = source.shape, target.shape
+        padding = ()
+        for x in range(len(source_shape)):
+            if source_shape[x] == 1:
+                padding = (0,0) + padding
+            else:
+                padding = (0,source_shape[x]*2 - target_shape[x]) + padding
+
+        if [i for i in padding if i > 0]:
+            target = pad(target, padding)
+
+        return source, target
+    
+    def set_tomograms(self, source_tomograms: int, target_tomograms: int) -> None:
+        self.source_tomograms = source_tomograms
+        self.target_tomograms = target_tomograms
+
+    def get_tomograms(self) -> tuple[int, int]:
+        return self.source_tomograms, self.target_tomograms
+    
+    def get_tensor_range(self) -> tuple[int|float, int|float]:
+        self.min_tensor, self.max_tensor = 0,0
+        for i in range(len(self.tomograms)):
+            for j in self.__getitem__(i):
+                if torch.max(j) > self.max_tensor:
+                    self.max_tensor = torch.max(j)
+                if torch.min(j) < self.min_tensor:
+                    self.min_tensor = torch.min(j)
+            
+        return self.min_tensor.item(), self.max_tensor.item()
---
+++ CustomTransform.py
@@ -0,0 +1,55 @@
+import torch
+import random
+import numpy as np
+from random import randint
+
+class RandomCrop3D(torch.nn.Module):
+    def forward(self, source, target, size:int = 5):  # we assume inputs are always structured like this
+        
+        source_x_size, target_x_size, x_start = self._get_random_size(source.shape[0], target.shape[0], size)
+        source_y_size, target_y_size, y_start = self._get_random_size(source.shape[1], target.shape[1], size)
+        source_z_size, target_z_size, z_start = self._get_random_size(source.shape[2], target.shape[2], size)
+
+        #print("source size: ", source_x_size, source_y_size, source_z_size, "from: ", source.shape)
+        #print("target size: ", target_x_size, target_y_size, target_z_size, "from: ", target.shape)
+        # Do some transformations. Here, we're just passing though the input
+        
+        return source[...,x_start:source_x_size, y_start:source_y_size, z_start:source_z_size], target[...,x_start*2:target_x_size, y_start*2:target_y_size, z_start*2:target_z_size]
+
+    def _get_random_size(self, source_size: int, target_size:int, size:int):
+        
+        source = randint(round(source_size/size), source_size)
+        width = randint(0,source_size - source)
+        target = source*2
+        if target > target_size:
+            target = target_size
+
+        return source+width, target+(width*2), width
+    
+class RandomFlip(torch.nn.Module):
+    def forward(self, source: torch.tensor, target:torch.tensor, chance: float|tuple = 0.33) -> torch.tensor:
+        #print(source.shape)
+        if isinstance(chance, float):
+            x_chance = chance
+            y_chance = chance
+            z_chance = chance
+        elif isinstance(chance, tuple):
+            x_chance = chance[0]
+            y_chance = chance[1]
+            z_chance = chance[2]
+
+        if self.get_random(x_chance):
+            source = np.flip(source, -1)
+            target = np.flip(target, -1)
+        if self.get_random(y_chance):
+            source = np.flip(source, -2)
+            target = np.flip(target, -2)
+        if self.get_random(z_chance):
+            source = np.flip(source, -3)
+            target = np.flip(target, -3)
+
+        return source, target
+
+    def get_random(self, chance:float) -> bool:
+        
+        return random.random() < chance
